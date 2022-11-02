66200c81f526fe1ebdf2190897f95b0459cd6bdc
Noah Lewis <lewisnoah592@gmail.com>
2022-11-01 19:54:40 -0500

    test
    test

--- JsSrc/Components/Movie/Movie.js
+++ JsSrc/Components/Movie/Movie.js
@@ -9,36 +9,42 @@ class Movie extends HTMLElement {
     async setupInnerHTML(movie, commentsJson, usernames, liked, userReview, movieId) {
         let result = "";
 
-        result += "<hr>";
-        result += `<p>Name: ${movie.name}</p>`;
+        result += '<div id="movie">';
+
+        result += `<h1 id="title">${movie.name}</h1>`;
+        // getting the img with movie
+        result += `<img id="movie-image" width="200" height="300" src="/images/${movie.image}"></img>`;
+        result += '<div id="movie-details">';
         result += `<p>Production Studio: ${movie.studio}</p>`;
         result += `<p>Length: ${movie.length}</p>`;
         result += `<p>Rating: ${movie.rating}</p>`;
         result += `<p>Director: ${movie.director}</p>`;
         result += `<p>Likes: ${movie.likes}</p>`;
+        result += '</div>';
         result += `Like<input id="liked" onclick="toggleLike()" type=checkbox ${(liked) ? 'checked' : ''}></input>`
-        result += `<p>Summary: ${movie.summary}</p>`;
+        result += `<h4 id="movie-summary">Summary: ${movie.summary}</h4>`;
 
-        // getting the img with movie
-        result += `<img width="200" height="300" src="/images/${movie.image}"></img>`;
-
-        result += "<hr>";
+        result += '</div>';
 
         // for user to sbumit a review
-        result += `<textarea id="review">${(!userReview) ? "" : userReview}</textarea>`;
+        result += '<h1>Write a review:</h1>';
+        result += `<textarea id="review-box">${(!userReview) ? "" : userReview}</textarea>`;
         result += `<button onclick="submitReview()">Submit Review</button>`;
         result += `<button onclick="deleteReview()">Delete Review</button>`;
 
+        result += '<div id="comments">';
 
         if (Array.isArray(commentsJson.moviescomments)) {
             let count = 0;
             for (let comment in commentsJson.moviescomments) {
-                result += "<hr>";
-                result += `<p>${usernames[count++] || 'Anonymous'}</p>`;
+                result += '<div class="comment">';
+                result += `<p class="comment-username">${usernames[count++] || 'Anonymous'}</p>`;
                 result += `<p>${commentsJson.moviescomments[comment].comment}</p>`;
-                result += "<hr>";
+                result += "</div>";
             }
         }
+
+        result += "</div>";
 
         return result;
     }
@@ -62,7 +68,7 @@ class Movie extends HTMLElement {
             //removing dummy comment from front of the array
             commentsJson.moviescomments.shift();
 
-            for (let i = 1; i < commentsJson.moviescomments.length; i++) {
+            for (let i = 0; i < commentsJson.moviescomments.length; i++) {
                 let userId = commentsJson.moviescomments[i].userId;
 
                 let userRes = await fetch(`${BACKEND_URL}UserServlet?userId=${userId}`);
